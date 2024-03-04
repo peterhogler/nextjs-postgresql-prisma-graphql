3ef9e00251abfeabedd2addd420d8a98da366a8c
Peter Hogler <me@peterhogler.dev>
2024-03-04 12:57:35 +0100

    update: fixed mobile responsivenss

--- app/components/feed/activity/activity-stream-filter.tsx
+++ app/components/feed/activity/activity-stream-filter.tsx
@@ -2,8 +2,8 @@ export default function ActivityStreamFilter() {
     return (
         <div className="sticky top-0 border-b border-x border-neutral-700 bg-black/70 max-h-min backdrop-blur-sm z-10">
             <div className="grid grid-cols-2">
-                <button className="hover:bg-neutral-500/20 h-full p-4">For you</button>
-                <button className="hover:bg-neutral-500/20 h-full p-4">Following</button>
+                <button className="hover:bg-neutral-500/20 h-full p-[0.75rem] lg:p-4">For you</button>
+                <button className="hover:bg-neutral-500/20 h-full p-[0.75rem] lg:p-4">Following</button>
             </div>
         </div>
     );
--- app/components/navigation-sidebar.tsx
+++ app/components/navigation-sidebar.tsx
@@ -14,8 +14,8 @@ export default function NavigationSidebar() {
     const { data: session, status } = useSession();
 
     return (
-        <div className=" flex flex-col items-center lg:items-start h-full pb-5 pt-2  lg:w-[275px] ">
-            <Link className="text-3xl lg:pl-3 lg:pr-5 py-2 px-2mb-3 lg:mb-4 font-bold" href="/">
+        <div className=" flex flex-col items-center lg:items-start h-full pb-5 pt-2 px-3 lg:px-0 lg:w-[275px] ">
+            <Link className="text-3xl lg:pl-3 lg:pr-5 py-2 px-2 mb-3 lg:mb-4 font-bold" href="/">
                 X
             </Link>
             <div className="space-y-5 text-xl">
@@ -43,7 +43,7 @@ export default function NavigationSidebar() {
             <button className="hidden lg:block font-bold text-xl bg-sky-500 w-4/5 py-2 mt-6 rounded-full disabled:opacity-40 disabled:cursor-not-allowed " disabled={!session}>
                 Post
             </button>
-            <div className="relative flex w-[98%] items-center justify-between mt-auto  px-2 pr-5 py-2 rounded-full hover:bg-neutral-900/40" onClick={() => setIsProfileExanded(!isProfileExpanded)}>
+            <div className="relative flex w-[98%] items-center justify-between mt-auto  lg:px-2 lg:pr-5 lg:py-2 rounded-full hover:bg-neutral-900/40" onClick={() => setIsProfileExanded(!isProfileExpanded)}>
                 {session ? (
                     <div className="flex flex-1 items-center gap-3 mt-auto ">
                         <div className="h-[45px] w-[45px] flex items-center justify-center relative rounded-full ">{session?.user?.image ? <Image className="rounded-full" src={session?.user?.image as string} layout="fill" objectFit="contain" alt="profile picture" /> : <FiUser size={35} />}</div>
@@ -60,18 +60,18 @@ export default function NavigationSidebar() {
                         <div className="h-[40px] w-[40px] flex items-center justify-center border-[2.5px] rounded-full ">
                             <FiUser size={35} />
                         </div>
-                        <div className="flex-1 flex items-center justify-between leading-tight text-left">
+                        <div className="hidden flex-1 lg:flex items-center justify-between leading-tight text-left">
                             <div>
                                 <p className=" font-bold">Guest</p>
                                 <p className="text-neutral-500">Click to login</p>
                             </div>
+                            <FiMoreHorizontal size={22} />
                         </div>
-                        <FiMoreHorizontal size={22} />
                     </div>
                 )}
 
                 {isProfileExpanded && (
-                    <div className="absolute w-full bottom-[4.5rem] rounded-2xl py-4 shadow-white/20 shadow-sm">
+                    <div className="absolute w-[max-content] lg:w-full bottom-[4.5rem] rounded-2xl py-4 shadow-white/20 shadow-sm">
                         {session ? (
                             <div className="px-4 py-2 hover:bg-neutral-900/40 font-bold cursor-pointer" onClick={() => signOut()}>
                                 Log out from {session?.user?.name}
