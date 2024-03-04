2e172a4c2e91c39370b3ed1a09c7ba945d6b9b9f
Peter Hogler <me@peterhogler.dev>
2024-03-04 13:14:27 +0100

    update fixed not-signed-in tsx

--- app/components/not-signed-in.tsx
+++ app/components/not-signed-in.tsx
@@ -5,7 +5,7 @@ export default function NotSignedInMessage() {
         <div className="py-8 px-8 border-b border-x border-neutral-700 space-y-4">
             <h1 className="text-2xl font-bold">You are currently not signed in 🥹</h1>
             <p className="text-neutral-500">
-                You are not currently signed in. Which means your account is read-only mode. Please sign in to be able to create posts. You can sign in here{" "}
+                You are not currently signed in. Which means your account is read-only mode. Please sign in to be able to create posts.
                 <Link className="text-sky-500" href="/login">
                     login page
                 </Link>
